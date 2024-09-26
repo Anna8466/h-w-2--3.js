e6071ea96bedb258c081722cb0238f875654f840
Anna Tr <anna.trandafil14@gmail.com>
2024-09-26 21:06:28 +0200

    h-w-2-3-2

--- h-w-2-3.js
+++ h-w-2-3.js
@@ -1,30 +1,17 @@
 const players = [
-    {
-    "id": 1,
-    "name": "Ivan",
-    "scorePoints": 500
-    },
-    {
-    "id": 2,
-    "name": "Petr",
-    "scorePoints": 3600
-    },
-    {
-    "id": 3,
-    "name": "Vadim",
-    "scorePoints": 3433
-    },
-    {
-    "id": 4,
-    "name": "Olga",
-    "scorePoints": 2356
-    },
-    ];
-    
-    //цикл    
-    //Math
-    //свойство через точку
-
+    { name: "Player1_1", scorePoints: 0 },
+    { name: "Player2_1", scorePoints: 0 },
+    { name: "Player1_2", scorePoints: 50 },
+    { name: "Player2_2", scorePoints: 75 },
+    { name: "Player3_1", scorePoints: 60 },
+    { name: "Player1_3", scorePoints: -50 },
+    { name: "Player2_3", scorePoints: -30 },
+    { name: "Player3_2", scorePoints: -10 },
+    { name: "Player1_4", scorePoints: 50.5 },
+    { name: "Player2_4", scorePoints: 75.25 },
+    { name: "Player3_3", scorePoints: 60.75 },
+];
+  
     let arrScores = [];
 
 for(index in players) {
